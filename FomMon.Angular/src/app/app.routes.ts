85fc9d1da12bc4e0da39acce5c08a56fde8d27db
jackwill <jackson.r.williams@gmail.com>
2025-10-15 18:18:15 -0700

    remove legacy js map

--- FomMon.Angular/src/app/app.routes.ts
+++ FomMon.Angular/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import {MapComponent} from "../components/map/map";
 import {HomeComponent} from "../components/home/home";
 import {NgxMap} from '../components/ngx-map/ngx-map';
 import {AreaWatchList} from '../components/area-watch/area-watch-list/area-watch-list';
@@ -8,7 +7,6 @@ import {AreaWatchAddComponent} from '../components/area-watch/area-watch-add/are
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
-  { path: 'map', component: MapComponent},
   { path: 'ngx-map', component: NgxMap },
   { path: 'area-watch-list', component: AreaWatchList}, // TODO auth
   { path: 'area-watch-add', component: AreaWatchAddComponent}
