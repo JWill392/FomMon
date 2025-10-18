e981480fb8da0884ac46d0ce1304e68e2ea7936b
jackwill <jackson.r.williams@gmail.com>
2025-10-18 09:04:46 -0700

    missed minio files

---
+++ MinIO.MinIO.Hosting/MinioResource.cs
@@ -0,0 +1,31 @@
+﻿using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+
+namespace MinIO.MinIO.Hosting;
+
+public sealed class MinioResource(string name, 
+    IResourceBuilder<ParameterResource> rootUser, 
+    IResourceBuilder<ParameterResource> rootPassword)
+    : ContainerResource(name), IResourceWithConnectionString
+{
+    internal const string HttpEndpointName = "http";
+    internal const string HttpAdminEndpointName = "admin";
+
+    public ParameterResource RootUser { get; } = rootUser.Resource;
+    public ParameterResource RootPassword { get; } = rootPassword.Resource;
+
+    private EndpointReference? _httpEndpointReference;
+    private EndpointReference? _httpAdminEndpointReference;
+
+    public EndpointReference HttpEndpoint =>
+        _httpEndpointReference ??= new EndpointReference(this, HttpEndpointName);
+
+    public EndpointReference HttpAdminEndpoint =>
+        _httpAdminEndpointReference ??= new EndpointReference(this, HttpAdminEndpointName);
+
+    public ReferenceExpression ConnectionStringExpression => 
+        ReferenceExpression.Create(
+            $"Endpoint={HttpEndpoint.Property(EndpointProperty.HostAndPort)};AccessKey={RootUser};SecretKey={RootPassword}"
+        );
+    
+}
